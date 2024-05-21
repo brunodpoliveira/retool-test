cf95ba4651843ab491fd249d923e79f2b73568f0
Bruno Daniel <brunodp.oliveira@gmail.com>
2024-05-21 16:47:36 -0300

    Fix: added User-Agent to avoid 403 error

--- index.js
+++ index.js
@@ -22,7 +22,12 @@ app.get('/scrape/:zipCode', async (req, res) => {
   const url = `https://www.realtor.com/realestateandhomes-search/${zipCode}`;
 
   try {
-    const { data } = await axios.get(url);
+    const { data } = await axios.get(url, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+      }
+    });
+
     const $ = cheerio.load(data);
     let listings = [];
 
