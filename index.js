677de5ec2d0ef991a141611651eb23182645505b
Bruno Daniel <brunodp.oliveira@gmail.com>
2024-05-21 22:53:27 -0300

    Feat: ScraperAPI method

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const axios = require('axios');
+const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 const cors = require('cors');
 const app = express();
@@ -7,35 +7,30 @@ const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
-
+const SCRAPER_API_KEY = 'c7a210349447adac07a18eff792c6896';
 app.get('/', (req, res) => {
   res.send('Welcome to the Realtor.com Scraper API. Use the /scrape/:zipCode endpoint.');
 });
 
 app.get('/scrape/:zipCode', async (req, res) => {
   const zipCode = req.params.zipCode;
-  const url = `https://www.realtor.com/realestateandhomes-search/${zipCode}`;
+
+  const url = `https://api.scraperapi.com/?api_key=${SCRAPER_API_KEY}&url=https://www.realtor.com/realestateandhomes-search/${zipCode}`;
 
   try {
     console.log(`Fetching data from URL: ${url}`);
-    const { data } = await axios.get(url, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
-      }
-    });
+    const response = await fetch(url);
+    const data = await response.text();
     const $ = cheerio.load(data);
     const scriptContent = $('#__NEXT_DATA__').html();
     if (!scriptContent) {
       console.error('Data script not found');
       return res.status(404).send('Data script not found');
     }
-
     let jsonData;
-
     try {
       jsonData = JSON.parse(scriptContent);
     } catch (parseError) {
-
       console.error('Error parsing JSON data:', parseError.message);
       return res.status(500).send('Error parsing JSON data');
     }
@@ -47,7 +42,6 @@ app.get('/scrape/:zipCode', async (req, res) => {
     }
 
     const listings = properties.map(property => ({
-
       address: property.location.address.line,
       price: property.list_price,
       beds: property.description.beds,
@@ -60,9 +54,9 @@ app.get('/scrape/:zipCode', async (req, res) => {
   } catch (error) {
     const errorMsg = error.message;
     const responseStatus = error.response ? error.response.status : 'Unknown';
-    const responseData = error.response ? error.response.data : 'No response data';
+    //const responseData = error.response ? error.response.data : 'No response data';
     console.error('Error fetching data:', errorMsg, 'Response status:', responseStatus);
-    console.error('Response data:', responseData);
+    //console.error('Response data:', responseData);
     res.status(500).send(`Error fetching data: ${errorMsg}`);
   }
 });
